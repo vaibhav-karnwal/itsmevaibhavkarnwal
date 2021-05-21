630bdddd220c9d869cbcf8c69d4a1bd3ca253b0a
Vaibhav Karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2021-05-21 17:14:37 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,19 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "vaibhavkarnwal2812@mail.com";
+$name = 'name';
+$email= 'email';
+$message= 'message';
+$to = "vaibhavkarnwal2812@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = 'From: <anmolkarnwal2812@gmail.com>' . "\r\n" .
-"CC: vaibhavkarnwal2812@gmail.com";
+"Cc : vaibhavkarnwal2812@gmail.com" . "\r\n";
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    mail($to ,$subject ,$txt ,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+if(mail($to ,$subject ,$txt ,$headers)){
+    echo "Mail Sent";   
+}else{
+    echo "Mail Failed";
 ?>
